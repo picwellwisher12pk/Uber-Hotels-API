48976a4c47ce988234f579be5da4f798eb8cce20
Amir Hameed <picwellwisher12pk@gmail.com>
2020-02-24 22:39:11 +0000

    Factory Seeder for hotels

--- database/seeds/HotelsTableSeeder.php
+++ database/seeds/HotelsTableSeeder.php
@@ -14,13 +14,14 @@ class HotelsTableSeeder extends Seeder
     {
         Hotel::truncate();
         $faker = \Faker\Factory::create();
+        $faker->addProvider(new Xvladqt\Faker\LoremFlickrProvider($faker));
         for ($i = 0; $i < 5; $i++) {
             Hotel::create([
                 'name' => $faker->company,
                 'email' => $faker->unique()->safeEmail,
                 'phone' => $faker->phoneNumber,
                 'address' => $faker->address,
-                'image' => $faker->imageUrl($width = 640, $height = 480,'cats'),
+                'image' => $faker->imageUrl($width = 640, $height = 480, ['hotels', 'buildings'],true),
                 'longitude' => $faker->longitude($min = 74.39, $max = 74.40),
                 'latitude' => $faker->latitude($min = 31.47, $max = 31.56)
 
