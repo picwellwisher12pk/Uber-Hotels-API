b6d789a41285cd57516678e6fc7cac3a003a4bf7
Amir Hameed <picwellwisher12pk@gmail.com>
2020-04-17 15:35:03 +0500

    Booking Resource

--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'checkin', 'checkout','phone','email','numberOfPeople','room_id','hotel_id'
+    ];
 }
--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Booking;
+use App\Hotel;
+use App\Room;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
@@ -35,7 +37,11 @@ class BookingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $booking = Booking::create($request->all());
+        $hotel_id = $request->input('hotel_id');
+        $room_id = $request->input('room_id');
+        Room::where('hotel_id',$hotel_id)->where('id',$room_id)->update(['is_available' => 0]);
+        return response()->json($booking, 201);
     }
 
     /**
