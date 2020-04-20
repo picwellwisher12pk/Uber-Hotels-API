3b54fe523cd988a9d145a023c4c9307197b39dd9
Amir Hameed <picwellwisher12pk@gmail.com>
2020-04-20 13:27:31 +0500

    Some refactoring,and added room-count to hotel listing methods. Removed unused code.

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -3,12 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Hotel;
-use App\Room;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Collection;
-
-use function PHPSTORM_META\type;
 
 class HotelController extends Controller
 {
@@ -31,51 +26,33 @@ class HotelController extends Controller
         }
       }
 
-
-    private function distance2($latitude1, $longitude1, $latitude2,  $longitude2)
-    {
-           $long1 = deg2rad($longitude1);
-           $long2 = deg2rad($longitude2);
-           $lat1 = deg2rad($latitude1);
-           $lat2 = deg2rad($latitude2);
-
-           //Haversine Formula
-           $dlong = $long2 - $long1;
-           $dlati = $lat2 - $lat1;
-
-           $val = pow(sin($dlati/2),2)+cos($lat1)*cos($lat2)*pow(sin($dlong/2),2);
-
-           $res = 2 * asin(sqrt($val));
-
-           $radius = 3958.756/1000;
-
-           return ($res*$radius);
-    }
     public function index()
     {
-        return Hotel::all();
+        return Hotel::withCount('rooms')->get();
     }
 
     public function indexNearBy($longitude,$latitude)
     {
         $this->latitude = $latitude;
         $this->longitude = $longitude;
-        $hotels = Hotel::all();
+        $hotels = Hotel::withCount('rooms')->get();
         $hotelsNearBy = $hotels->map(function($hotel){
             $distance = $this->distanceTo($this->latitude, $this->longitude, $hotel['longitude'],$hotel['latitude']);
             if( $distance < 5.0) {
                 $hotel['distance']=$distance;
                 return $hotel;
             }
-
         })->sortBy('distance');
         $hotelsNearBy = $hotelsNearBy->filter()->flatten();
         return $hotelsNearBy;
     }
 
-    public function show(Hotel $hotel)
+    public function show($id)
     {
-        $hotel["rooms"]= $hotel->rooms;
+        $hotel = Hotel::with('rooms')->findOrFail($id);
+        if(is_null($hotel)){
+            return response()->json(null,404);
+        }
         return $hotel;
     }
 
@@ -93,7 +70,7 @@ class HotelController extends Controller
         return response()->json($hotel, 200);
     }
 
-    public function delete(Request $request, Hotel $hotel)
+    public function delete(Hotel $hotel)
     {
         $hotel->delete();
 
